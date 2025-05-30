e263323b67c4edb85aad8057f43ae09cc3e4b8e8
dsw43089 <dsw43089@student.dsw.edu.pl>
2025-05-30 09:14:59 -0400

    Dodano obsluge flagi --logs

--- git-gui/skrypt.sh
+++ git-gui/skrypt.sh
@@ -1,3 +1,7 @@
-if [[ "$1" == "--date" ]]; then
-    date
+if [[ "$1" == "--logs" ]]; then
+    for ((i=1; i<=100; i++)); do
+        filename="log$i.txt"
+        echo -e "Nazwa pliku: $filename\nSkrypt: $0\nData: $(date)" > "$filename"
+    done
 fi
+
