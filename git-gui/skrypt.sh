87508044366217bdaf9213cb45c8040e6a473f6c
dsw43089 <dsw43089@student.dsw.edu.pl>
2025-06-05 12:02:56 -0400

    Dodano skrocone flagi -d, -l i -h

--- git-gui/skrypt.sh
+++ git-gui/skrypt.sh
@@ -1,17 +1,20 @@
-if [[ "$1" == "--date" ]]; then
+#!/bin/bash
+
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
     date
 fi
 
-if [[ "$1" == "--logs" ]]; then
-    count=${2:-100}  # domyślnie 100
+if [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+    count=${2:-100}
     for ((i=1; i<=count; i++)); do
         filename="log$i.txt"
         echo -e "Nazwa pliku: $filename\nSkrypt: $0\nData: $(date)" > "$filename"
     done
 fi
 
-if [[ "$1" == "--help" ]]; then
-    echo "--date : wyświetla dzisiejszą datę"
-    echo "--logs [n] : tworzy n plików log"
-    echo "--help : wyświetla pomoc"
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    echo "--date, -d : wyświetla dzisiejszą datę"
+    echo "--logs [n], -l [n] : tworzy n plików log"
+    echo "--help, -h : wyświetla tę pomoc"
 fi
+
